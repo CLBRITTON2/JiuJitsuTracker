4e75dcb8140b35f799b43954f6dc773e01dd16e7
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-11 15:35:28 -0400

    Create JiuJitsuLog database

---
+++ JiuJitsuTracker/Migrations/20221011193049_AddCategoryToDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JiuJitsuTracker.Migrations
+{
+    public partial class AddCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BeltColor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClassUniformType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClassFocus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MatTime = table.Column<int>(type: "int", nullable: false),
+                    TotalMatTime = table.Column<int>(type: "int", nullable: false),
+                    ClassLogDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classes");
+        }
+    }
+}
--- JiuJitsuTracker/Program.cs
+++ JiuJitsuTracker/Program.cs
@@ -1,7 +1,15 @@
+using JiuJitsuTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure DbContext with the connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
