9a8ce48d299b7d7d877595d9e9c639b73db752cf
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-11 15:00:34 -0400

    Initial commit

---
+++ JiuJitsuTracker/Models/ClassInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace JiuJitsuTracker.Models
+{
+    public class ClassInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UniformClassType { get; set; }
+        public string BeltColor { get; set; }
+        public string ClassFocus { get; set; }
+        public DateTime ClassLogDateTime { get; set; } = DateTime.Now;
+    }
+}
