7df937e770c3f4cf1a2f61aeecc64cd82af25656
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-11 16:04:36 -0400

    Create log layout page and link categories to db

---
+++ JiuJitsuTracker/Controllers/JiuJitsuLogController.cs
@@ -0,0 +1,21 @@
+﻿using JiuJitsuTracker.Data;
+using JiuJitsuTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JiuJitsuTracker.Controllers
+{
+    public class JiuJitsuLogController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public JiuJitsuLogController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            // Go to database, retrieve classes, convert them to a list
+            IEnumerable<ClassInfo> objectCategoryList = _db.Classes;
+            return View(objectCategoryList);
+        }
+    }
+}
