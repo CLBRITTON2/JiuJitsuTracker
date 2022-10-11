a58c82d2991d6bccbd342021fb0adf3caf66d6ae
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-11 15:05:32 -0400

    Adjust properties for class info model

--- JiuJitsuTracker/Models/ClassInfo.cs
+++ JiuJitsuTracker/Models/ClassInfo.cs
@@ -4,9 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string UniformClassType { get; set; }
         public string BeltColor { get; set; }
+        public string ClassUniformType { get; set; }
         public string ClassFocus { get; set; }
+        public int MatTime { get; set; }
+        public int TotalMatTime { get; set; }
         public DateTime ClassLogDateTime { get; set; } = DateTime.Now;
     }
 }
