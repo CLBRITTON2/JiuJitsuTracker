fa2783c95066c12ea06b6acb9d70537b0d297545
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-17 10:39:20 -0400

    Adjust display names for categories while creating log entries
    Add range and custom error message to mat time entry

--- JiuJitsuTracker/Models/ClassInfo.cs
+++ JiuJitsuTracker/Models/ClassInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace JiuJitsuTracker.Models
 {
@@ -6,11 +7,17 @@ namespace JiuJitsuTracker.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Name (optional)")]
         public string Name { get; set; }
+        [DisplayName("Belt Color")]
         public string BeltColor { get; set; }
+        [DisplayName("Uniform (gi or no gi)")]
         public string ClassUniform { get; set; }
         [Required]
+        [DisplayName("Class Focus")]
         public string ClassFocus { get; set; }
+        [DisplayName("Mat Time")]
+        [Range(0, 24, ErrorMessage ="There are 24 hours in a day")]
         public double MatTime { get; set; }
         public double TotalMatTime { get; set; }
         public DateTime ClassLogDateTime { get; set; } = DateTime.Now;
