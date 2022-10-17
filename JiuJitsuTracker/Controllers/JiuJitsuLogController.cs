0890c317b234c8030cc88bb7f0f752d7c60ba3f8
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-17 11:20:14 -0400

    Add edit button and functionality to log UI

--- JiuJitsuTracker/Controllers/JiuJitsuLogController.cs
+++ JiuJitsuTracker/Controllers/JiuJitsuLogController.cs
@@ -14,8 +14,8 @@ namespace JiuJitsuTracker.Controllers
         public IActionResult Index()
         {
             // Go to database, retrieve classes, convert them to a list
-            IEnumerable<ClassInfo> objectCategoryList = _db.Classes;
-            return View(objectCategoryList);
+            IEnumerable<ClassInfo> objectClassList = _db.Classes;
+            return View(objectClassList);
         }
         // Get action method
         public IActionResult Create()
@@ -33,6 +33,40 @@ namespace JiuJitsuTracker.Controllers
             {
                 // Adds user input class info to the database then saves info to the db
                 _db.Classes.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        // Get action method
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                // if ID is null or is = 0 it's invalid
+                return NotFound();
+            }
+
+            var classFromDb = _db.Classes.Find(id);
+
+            if (classFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(classFromDb);
+        }
+
+        // Post action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ClassInfo obj)
+        {
+            // Server side validation
+            if (ModelState.IsValid)
+            {
+                // Updates properties in DB when user uses the update button
+                _db.Classes.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
