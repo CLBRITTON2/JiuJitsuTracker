d65462f2c1a554fe08c9e88ad74099501888bb4a
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-24 16:38:28 -0400

    Create interface for ClassInfo model

---
+++ JiuJitsuTracker.DataAccess/Repository/IRepository/IClassInfoRepository.cs
@@ -0,0 +1,15 @@
+﻿using JiuJitsuTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuJitsuTracker.DataAccess.Repository.IRepository
+{
+    public interface IClassInfoRepository : IRepository<ClassInfo>
+    {
+        void Update(ClassInfo obj);
+        void Save();
+    }
+}
