167b9bc3fc58c8c873e16a5d3fccd3a1bf2aac61
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-24 16:43:35 -0400

    Create and implement class info repository

---
+++ JiuJitsuTracker.DataAccess/Repository/ClassInfoRepository.cs
@@ -0,0 +1,28 @@
+﻿using JiuJitsuTracker.DataAccess.Repository.IRepository;
+using JiuJitsuTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuJitsuTracker.DataAccess.Repository
+{
+    public class ClassInfoRepository : Repository<ClassInfo>, IClassInfoRepository
+    {
+        private ApplicationDbContext _db;
+        public ClassInfoRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(ClassInfo obj)
+        {
+            _db.Classes.Update(obj);
+        }
+    }
+}
